a584a9298039b27c0963c9e6cd23b99bb00d6cbd
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-12-16 09:21:34 -0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:firebaseapp/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
