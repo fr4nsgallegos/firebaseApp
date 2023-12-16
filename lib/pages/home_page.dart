4dc0dd5268120c78e679016c0fe91f4b2483d735
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-12-16 10:00:47 -0500

    init config firebase

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: const Text(
+            "FirebaseApp",
+          ),
+          centerTitle: true,
+        ),
         body: Column(),
       ),
     );
