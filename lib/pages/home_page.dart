40a74678cf31fa048fe187c5223239cfaaed6cac
fr4nsgallegos <fransgallegosmendoza@gmail.com>
2023-12-16 11:26:41 -0500

    firebase init

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    CollectionReference usersReference =
+        FirebaseFirestore.instance.collection('USERS');
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
